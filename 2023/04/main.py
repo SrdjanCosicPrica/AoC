8b453624aee1cf731df4b505a9a401573c0013ec
SrdjanCosicPrica <15933115+SrdjanCosicPrica@users.noreply.github.com>
2023-12-06 18:22:26 +0100

    Add solution for 04

---
+++ 2023/04/main.py
@@ -0,0 +1,30 @@
+f = open('input.txt')
+
+tot = 0
+tot_scratch_cards = 0
+COPIES = {}
+
+
+def increment_card_copy(index, amount):
+  global tot_scratch_cards
+  COPIES[index] = COPIES.get(index, 0) + amount
+  tot_scratch_cards += amount
+
+
+for i, line in enumerate(f):
+  [card, numbers] = line.split(':')
+  [winning_numbers, my_numbers] = numbers.split('|')
+  winning_numbers = [number.strip() for number in winning_numbers.split(' ') if number]
+  my_numbers = [number.strip() for number in my_numbers.split(' ') if number]
+
+  increment_card_copy(i, 1)
+
+  matches = [number for number in winning_numbers if number in my_numbers]
+  if len(matches):
+    tot += 2**(len(matches) - 1)
+    for x in range(1, len(matches) + 1):
+      increment_card_copy(i + x, COPIES[i])
+
+
+print('part1', tot)
+print('part2', tot_scratch_cards)
